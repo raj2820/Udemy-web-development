967cd6f022798ffca305ba7092e5d3b86f89ce5f
Raj_Shinde <rajshinde2820@gmail.com>
2023-02-19 20:15:19 +0530

    adding Veu events

---
+++ Vue learning/Events/app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods:{
+    add(){
+      this.counter=this.counter+1;
+    },
+    sub(){
+      this.counter=this.counter-1;
+    }
+  }
+});
+
+app.mount('#events');
