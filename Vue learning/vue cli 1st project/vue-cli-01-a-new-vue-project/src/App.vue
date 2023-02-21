40a01dadd4b4935d635dd48e7f7c976d0ec4260d
Raj Shinde <rajshinde2820@gmail.com>
2023-02-21 13:38:42 +0530

    added vue components based app

---
+++ Vue learning/vue cli 1st project/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,157 @@
+<template> 
+<section>
+   <header><h1>My Friends</h1></header> 
+   <new-friend @add-contact="addContact"></new-friend>
+    <ul>
+        <friend-contact 
+        v-for="friend in friends"
+        :id="friend.id"
+        :key="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="friend.isFavorite"
+        @toggle-favourite="toggleFavouriteStatus"
+        @delete="deleteContact"
+        ></friend-contact>
+        
+    </ul>
+    
+</section>
+</template>
+<script>
+import FriendContact from './components/FriendContact.vue'
+import NewFriend from './components/NewFriend.vue';
+export default{
+  components: { FriendContact, NewFriend },
+    data(){
+        return{
+            friends:[
+                {
+                    id:'Raj',
+                    name:'Raj Shinde',
+                    phone:'01233454656',
+                    email:'raj@ymail.com',
+                    isFavorite:true
+                },
+                {
+                    id:'Yo',
+                    name:'Yo YO',
+                    phone:'6578900987',
+                    email:'YoYo@ymail.com',
+                    isFavorite:false
+                },
+                {
+                    id:'Jack',
+                    name:'Jack YO',
+                    phone:'6578900987',
+                    email:'YoYo@ymail.com',
+                    isFavorite:true
+                },
+                {
+                    id:'Sean',
+                    name:'Sean YO',
+                    phone:'6578900987',
+                    email:'YoYo@ymail.com',
+                    isFavorite:false
+                }
+            ]
+        }
+    },methods:{
+        toggleFavouriteStatus(friendId){
+        const identifiedFriend =    this.friends.find(friend =>friend.id=== friendId);
+            identifiedFriend.isFavorite=!identifiedFriend.isFavorite
+        },
+        addContact(name,phone,email){
+            const newFriendContact={
+                id:new Date().toISOString(),
+                name:name,
+                phone:phone,
+                email:email
+            };
+            this.friends.push(newFriendContact)
+        },deleteContact(friendId){
+            this.friends=this.friends.filter(friend=>friend.id!==friendId);
+            
+        }
+    }
+}
+</script>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Jost', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li ,
+#app form{
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}</style>
